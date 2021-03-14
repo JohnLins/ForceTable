fa9330dc0eb404162cd1cc55f099c528bb94fc20
John Lins <johnpaullins@gmail.com>
2021-03-14 13:07:23 -0700

    Fix polar problem

--- main.c
+++ main.c
@@ -11,6 +11,24 @@ typedef struct Weight {
     float theta;
 } Weight;
 
+void update(Weight *weights){
+    weights[0].mass = 50;
+    weights[1].mass = 50;
+    weights[2].mass = 110;
+    
+    weights[0].theta = 190 * -(PI/180);
+    weights[1].theta = 340 * -(PI/180);
+    weights[2].theta = 360 * -(PI/180);
+    
+    for(int i = 0; i < 3; i++){
+        printf("Mass of weight %d : ", i);
+        scanf("%f", &weights[i].mass);
+        printf("Angle of weight %d : ", i);
+        scanf("%f", &weights[i].theta);
+        weights[i].theta *= (-PI/180);
+    }
+}
+
 int main(void)
 {
     const int screenWidth = 1000;
@@ -24,14 +42,7 @@ int main(void)
         scanf("%f", &weights[i] -> theta);   
     }*/
     
-    weights[0].mass = 50;
-    weights[1].mass = 100;
-    weights[2].mass = 30;
-    //weights[3].mass = 80;
-    
-    weights[0].theta = 10 * -(PI/180);
-    weights[1].theta = 90 * -(PI/180);
-    weights[2].theta = 380 * -(PI/180);
+    update(weights);
     //weights[3].theta = 290 * -(PI/180);
     
     
@@ -39,29 +50,60 @@ int main(void)
     float yTotal = 0.0;
     for(int i = 0; i < 3; i++)
     {
-        xTotal += weights[i].mass * cosf(weights[i].theta * (180/PI));
-        yTotal += weights[i].mass * sinf(weights[i].theta * (180/PI));
+        xTotal += weights[i].mass * cosf(-weights[i].theta);
+        yTotal += weights[i].mass * sinf(-weights[i].theta);
     }     
+    //xTotal = -xTotal;
     //yTotal = -yTotal;
-
+   
+    
+   printf("R_x: %f \n", xTotal);
+   printf("R_y: %f \n", yTotal);
+    
         
-    printf("R_x: %f \n", xTotal);
-    printf("R_y: %f \n", yTotal);
-    
-    
-    printf("R mass: %f \n", sqrt(pow(xTotal,2) + pow(yTotal,2)));
-    printf("R theta - : %f \n", 1/tanf(xTotal/xTotal) * (180/PI) );
     
     weights[3].mass = sqrt(pow(xTotal,2) + pow(yTotal,2));
-    weights[3].theta = (1/tanf(yTotal/xTotal)) * -(180/PI);
+    
+    
+    
+    //printf("RAD: %f ", (1/tanf(yTotal/xTotal)));
+    
+    weights[3].theta = -(atanf(yTotal/xTotal));
+     weights[3].theta -= PI;
+    
+    //printf("BEFORE ADD: %f ", weights[3].theta);
     
     if(xTotal > 0){
         if(yTotal < 0){
-            weights[3].theta = weights[3].theta + 360.0;
+            weights[3].theta += 2*PI;
         }
     } else {
-        weights[3].theta = weights[3].theta + 180.0;
+        weights[3].theta += PI;
     }
+    
+   // weights[3].theta += PI/2;
+ //  weights[3].theta = weights[3].theta * (180/PI);
+ //weights[3].theta += 1;
+  //weights[3].theta = -1 * weights[3].theta;
+ 
+ 
+ 
+ //((3/4) * PI) - weights[3].theta - ((3/4) * PI);
+ 
+    
+    printf("R mass: %f \n", weights[3].mass);
+    printf("R theta - : %f \n", weights[3].theta);
+    
+    
+   /* if(xTotal > 0){
+        if(yTotal < 0){
+            printf("---- %f", ((1/tanf(xTotal/xTotal)) * -(180/PI)) - 360);
+        }
+    } else {
+        printf("---- %f", ((1/tanf(xTotal/xTotal)) * -(180/PI)) - 180);
+    }*/
+    
+    
     
     /*float Rec2Polar(float x, float y){
         
@@ -125,14 +167,13 @@ int main(void)
                 
             
             
-                for(int i = 0; i < 4; i++){
-                    DrawLineV((Vector2){ R*cosf(weights[i].theta) + screenWidth/2, R*sinf(weights[i].theta) + screenHeight/2}, agentPos, BLACK);
-                }
                 /*DrawLineV((Vector2){screenWidth/2, screenHeight}, agentPos, BLACK);
                 DrawLineV((Vector2){0, screenHeight/2}, agentPos, BLACK);
                 DrawLineV((Vector2){screenWidth, screenHeight/2}, agentPos, BLACK);*/
                 
-              
+              for(int i = 0; i< 4; i++){
+                  DrawLineV((Vector2){ R*cosf(weights[i].theta) + screenWidth/2, R*sinf(weights[i].theta) + screenHeight/2}, agentPos, BLACK);
+              }
                
                 
                
@@ -145,13 +186,17 @@ int main(void)
                     sprintf(thetaDisplay, "%f", weights[i].theta * -(180/PI));
                     sprintf(massDisplay, "%f", weights[i].mass);
                
+                    
                     DrawCircleV((Vector2){ R*cosf(weights[i].theta) + screenWidth/2, R*sinf(weights[i].theta) + screenHeight/2}, weights[i].mass, BLACK);
+                    
                     //DrawCircleLines(R*cosf(weights[i].theta) + screenWidth/2, R*sinf(weights[i].theta) + screenHeight/2, weights[i].mass, BLUE);
                     //DrawCircleV((Vector2){ R*cosf(weights[2].theta), R*sinf(weights[2].theta)}, weights[2].mass, BLACK);
                     DrawText(thetaDisplay, (R*cosf(weights[i].theta) + screenWidth/2),  (R*sinf(weights[i].theta) + screenHeight/2 - 40), 20, RED);
                     DrawText(massDisplay, (R*cosf(weights[i].theta) + screenWidth/2),  (R*sinf(weights[i].theta) + screenHeight/2 + 40), 20, BLUE);
                     
                 }
+                
+                 //DrawCircleV((Vector2){ R*cosf(xTotal), R*sinf(yTotal) + screenHeight/2}, weights[3].mass, RED);
                 
                 if(agentPos.x > screenWidth/2){
                     agentPos.x -= 0.6;
